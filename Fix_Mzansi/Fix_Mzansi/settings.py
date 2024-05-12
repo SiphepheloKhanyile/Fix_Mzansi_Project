05ed7b7ac00529859e573ea5fc072fa49fa02427
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-05-13 01:31:07 +0200

    fix: database change

--- Fix_Mzansi/Fix_Mzansi/settings.py
+++ Fix_Mzansi/Fix_Mzansi/settings.py
@@ -97,15 +97,12 @@ if os.getenv('DEBUG') == 'True':
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django_cockroachdb',
-            'NAME': os.environ.get("DB_NAME"),
-            'USER': os.environ.get("DB_USER"),
-            'PASSWORD': os.environ.get("DB_PASSWORD"),
-            'HOST': os.environ.get("DB_HOST"),
-            'PORT': os.environ.get("DB_PORT"),
-            'OPTIONS': {
-                'sslmode': 'require'
-            },
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get("POSTGRES_DATABASE"),
+            'USER': os.environ.get("POSTGRES_USER"),
+            'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+            'HOST': os.environ.get("POSTGRES_HOST"),
+            'PORT': os.environ.get("POSTGRES_PORT"),
         },
     }
     # DATABASES = {
