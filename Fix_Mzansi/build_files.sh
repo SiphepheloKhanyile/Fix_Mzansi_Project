4a4b91c1fd51cfea47aed3cf40315131d2e21efc
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-05-12 20:31:20 +0200

    fix: vercel fix

--- Fix_Mzansi/build_files.sh
+++ Fix_Mzansi/build_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # build_files.sh
-pythonp3.9 pip install -r requirements.txt
+python pip install -r requirements.txt
 
 sudo apt install curl -y
 
@@ -9,5 +9,5 @@ curl --create-dirs -o $HOME/.postgresql/root.crt "${CERT_URL}"
 export DATABASE_URL="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/fixmzansi_db?sslmode=verify-full"
 
 # make migrations 
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic --no-input
+python manage.py migrate 
+python manage.py collectstatic --no-input
