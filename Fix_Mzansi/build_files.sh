a96a9728a80b67ec7ffe45adcdf686361eb64e4d
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-05-12 06:24:05 +0200

    fix: Vercel Deployment Test, fixed vercel.json file

--- Fix_Mzansi/build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# build_files.sh
-pip install -r requirements.txt
-curl --create-dirs -o $HOME/.postgresql/root.crt 'https://cockroachlabs.cloud/clusters/f7afce05-224c-48ff-a0bb-9739c97439eb/cert'
-# make migrations
-python3.9 manage.py makemigrations 
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic --no-input
